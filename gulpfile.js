20a6cb98f30034cc3581ba7811b4798d0ca8c98b
madekot <mgolubcov@gmail.com>
2018-05-17 09:10:20 +0300

    Написал сборку

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,17 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var build = require("run-sequence");
+var del = require("del");
+var uglify = require("gulp-uglify");
+var pump = require("pump");
 
 gulp.task("style", function() {
   gulp.src("source/sass/style.scss")
@@ -14,13 +25,16 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("serve", function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -28,5 +42,79 @@ gulp.task("serve", ["style"], function() {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html"), ["posthtml"];
+});
+
+gulp.task("jsmin", function (cb) {
+  pump([
+        gulp.src(["build/js/*.js", "build/!js/*.min.js"]),
+        uglify(),
+        rename({suffix: ".min"}),
+        gulp.dest("build/js/")
+    ],
+    cb
+  );
+});
+
+gulp.task("imagemin", function() {
+  return gulp.src("build/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({
+        optimizationLevel: 3
+      }),
+      imagemin.jpegtran({
+        progressive: true
+      }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("build/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("svgstore", function() {
+  return gulp.src("build/img/{icon-vk-mobile,icon-insta-mobile,htmlacademy,icon-fb-mobile}.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("posthtml", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+      ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("picturefill", function() {
+  return gulp.src([
+      "node_modules/picturefill/dist/picturefill.js"
+    ])
+    .pipe(gulp.dest("build/js"));
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+      "source/fonts/**/*.{woff,woff2}",
+      "source/img/**/*.{png,jpg,svg}",
+      "source/js/**/*.js"
+      ], {
+        base: "source"
+      })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("del", function () {
+  return del("build");
+});
+
+gulp.task("build", function(done) {
+  build("del", "copy", "style", "picturefill", "jsmin", "svgstore", "posthtml", "imagemin", "webp",  done)
 });
